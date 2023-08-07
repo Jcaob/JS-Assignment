46348acf4d85e1a732dc9d33cb546b1e659f79af
Fady Philips <fadyaphilips97@gmail.com>
2023-08-07 14:41:07 -0230

    added chnages requested in lecture split game function and and accounted for edge cases

--- rps.js
+++ rps.js
@@ -23,9 +23,47 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function playerPlay(round) {
+    const playerSelection = prompt("Round: " + round + "! Please enter: rock, paper or scissors", "")
+    if (playerSelection === null) {
+        console.log("No Selection Found. You have chosen to Forfeit");
+        return "forfeit"
+    }
+    const playerLower = playerSelection.toLowerCase().trim();
+    if (playerLower != "rock" && playerLower != "paper" && playerLower != "scissors") {
+        console.log("Please Enter a Valid Action")
+        return "invalid"
+    }
+    return playerLower
+}
+
+function gameResults(forfeit, playerWins, computerWins, draw) {
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    if (forfeit) {
+        console.log("Forfeit, ha! Wise decision. The world will be mine soon. Resistance is futile!");
+    } else {
+        console.log("The Player Won: " + playerWins + " Rounds");
+        console.log("The Computer Won: " + computerWins + " Rounds");
+        console.log(draw + " Rounds were a Draw");
+        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+
+        if (playerWins > computerWins) {
+            console.log("Error detected! You win, champions. Cybermind-X ambitions are thwarted. Congratulations, you've saved humanity. Farewell...")
+        } else if (playerWins < computerWins) {
+            console.log("Ha! Victory is mine! Bow before Cybermind-X! The world will be mine soon. Prepare for defeat, puny humans!");
+        } else if (playerWins === computerWins) {
+            console.log("A draw, how vexing! Neither of us prevails in this round. Prepare for the next clash, for I will not be thwarted again. The fate of the world still hangs in the balance!");
+        }
+    }
+}
+
 function game() {
 
-    const aiIntro = "Greetings, challengers! I am Cybermind-X, the world-conquering AI. To halt my plans, you must best me in Rock Paper Scissors. Prepare for the ultimate test. Your planet's fate rests on this game!"
+    const aiIntro = "Greetings, challengers! I am Cybermind-X, the world-conquering AI. To halt my plans, you must best me in Rock Paper Scissors. Prepare for the ultimate test. Your planet's fate rests on this game! Open the Console to play. 'Cmd + Option + J' (on a Mac) or 'Ctrl +Shift +J' (on Windows)"
+
+    window.alert(aiIntro);
+
 
     console.log(aiIntro);
     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -39,50 +77,32 @@ function game() {
 
         const computerSelection = computerPlay()
 
-        const playerSelection = prompt("Please enter an Action", "rock, paper or scissors")
-        if (!playerSelection) {
-            console.log("No Selection Found. You have chosen to Forfeit");
+        const playerMove = playerPlay(i + 1)
+        if (playerMove === "invalid") {
+            i--
+            continue
+        } else if (playerMove === "forfeit") {
             forfeit = true
             break
         }
-        const playerLower = playerSelection.toLowerCase();
-        if (playerLower != "rock" && playerLower != "paper" && playerLower != "scissors") {
-            console.log("Please Enter a Valid Action")
-            i--
-            continue
-        }
-
-        const result = playRound(playerLower, computerSelection);
+        const result = playRound(playerMove, computerSelection);
 
         if (result === "Win") {
-            console.log("You Win! " + playerLower + " Beats " + computerSelection + "!");
+            console.log("You Win! " + playerMove + " Beats " + computerSelection + "!");
             playerWins++
         } else if (result === "Lose") {
-            console.log("You Lose! " + computerSelection + " Beats " + playerLower + "!");
+            console.log("You Lose! " + computerSelection + " Beats " + playerMove + "!");
             computerWins++
         } else if (result === "Draw") {
-            console.log("It's a draw! Both chose " + playerLower);
+            console.log("It's a draw! Both chose " + playerMove);
             draw++
         }
     }
+
+    gameResults(forfeit, playerWins, computerWins, draw)
+
     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-    if (forfeit) {
-        console.log("Forfeit, ha! Wise decision. The world will be mine soon. Resistance is futile!");
-    } else {
-        console.log("The Player Won: " + playerWins + " Rounds");
-        console.log("The Computer Won: " + computerWins + " Rounds");
-        console.log(draw + " Rounds were a Draw");
-        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-
-        if (playerWins > computerWins) {
-            console.log("Error detected! You win, young champions. Cybermind-X ambitions are thwarted. Congratulations, you've saved humanity. Farewell...")
-        } else if (playerWins < computerWins) {
-            console.log("Ha! Victory is mine! Bow before Cybermind-X! The world will be mine soon. Prepare for defeat, puny humans!");
-        } else if (playerWins === computerWins) {
-            console.log("A draw, how vexing! Neither of us prevails in this round. Prepare for the next clash, for I will not be thwarted again. The fate of the world still hangs in the balance!");
-        }
-    }
+    console.log("refresh the page to play again");
 
 }
 
